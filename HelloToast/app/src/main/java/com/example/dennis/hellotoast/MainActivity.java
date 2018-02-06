b477dc5b706396ac4a258a15ec82c826991cf5aa
dbdennot8 <dennisbmithamo@gmail.com>
2018-02-06 13:40:37 +0300

    Practicing with Linear, Constraint and Relative Layouts, and making Toast :)

---
+++ HelloToast/app/src/main/java/com/example/dennis/hellotoast/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.dennis.hellotoast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+//        Make Toast and show it when button is clicked
+        Toast myToast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_LONG);
+        myToast.show();
+    }
+
+    public void countUp(View view) {
+        mCount += 1;
+        if(mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+
+    }
+
+    public void reset(View view) {
+        mCount = 0;
+        mShowCount.setText(Integer.toOctalString(mCount));
+    }
+}
